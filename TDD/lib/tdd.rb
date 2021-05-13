8dcc4b2498c042beab4694a43596636c9f490bb7
Eric Tran <eric.tran119@gmail.com>
2021-05-13 15:18:00 -0400

    added stock picker

--- TDD/lib/tdd.rb
+++ TDD/lib/tdd.rb
@@ -22,10 +22,45 @@ class Array
     end
 end
 
+# def stock_picker(arr)
+#     least = []
+#     greatest = []
+#     (0...arr.length-1).each do |i|
+#         if arr[i][1] < arr[i + 1][1]
+#             least = arr[i]
+#         end 
+#     end
+#     least_index = arr.index(least)
+#     (least_index+1...arr.length-1).each do |i|
+#         if arr[i][1] > arr[i+1][1]
+#             greatest = arr[i]
+#         else
+#             greatest = arr[i+1]
+#         end
+#     end
+#     result = []
+#     result.push(least,greatest)
+#     return result
+# end
+
 def stock_picker(arr)
+    least = 0
+    greatest = 0
+    greatest_difference = 0
+    # arr.each do |i|
+    #     least << arr[i][1]
+    # end
     (0...arr.length).each do |i|
-        if arr[i][1] < arr[i + 1][1]
-            << arr[i][1]
-        end 
+        (1...arr.length).each do |j|
+            if j > i && greatest_difference < arr[j][1] - arr[i][1] 
+                least = i
+                greatest = j
+                greatest_difference = arr[j][1] - arr[i][1]
+            end
+        end
     end
+    [arr[least], arr[greatest]]
 end
+
+arr = [["a", 100], ["b", 200], ["c", 50], ["d", 500], ["e", 400]]
+p stock_picker(arr)
